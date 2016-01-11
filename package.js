98690c0d9f54c3535eff4ba41135055d3d1fdc3a
C. Scott Ananian <cscott@cscott.net>
2016-01-11 00:47:00 -0500

    Update to googleapis upstream v2.1.7.
    See
    https://github.com/google/google-api-nodejs-client/blob/master/MIGRATING.md
    for details on migrating to the new API.

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'cscottnet:googleapis',
   summary: 'Interface with Google APIs on Meteor server',
-  version: '0.4.7_1',
+  version: '2.1.7',
   git: 'https://github.com/cscott/meteor-googleapis.git'
 });
 
-Npm.depends({"googleapis": "0.4.7"});
+Npm.depends({"googleapis": "2.1.7"});
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
